b497165fef883dba35d0d18f7a0998fc2c9db838
linxiaolong <linxiaolong@damaiapp.com>
2016-04-18 15:53:57 +0800

    [Add]daily commit with date 2016年04月18日 15:53:57

--- src/ios/dmhuanxin.m
+++ src/ios/dmhuanxin.m
@@ -7,9 +7,6 @@
 }
 
 - (void)echo:(CDVInvokedUrlCommand*)command;
-- (void)init:(CDVInvokedUrlCommand*)command;
-- (void)conversation:(CDVInvokedUrlCommand*)command;
-- (void)chart:(CDVInvokedUrlCommand*)command;
 
 @end
 
@@ -29,55 +26,6 @@
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
-
-// 初始化接口
-- (void)init:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* message = [command.arguments objectAtIndex:0];
-
-    if (message != nil && [message length] > 0) {
-        message = @"来自iOS的init消息......";
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-// 进入聊天列表接口
-- (void)conversation:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* message = [command.arguments objectAtIndex:0];
-
-    if (message != nil && [message length] > 0) {
-        message = @"来自iOS的conversation消息......";
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-// 启动单聊页面
-- (void)chart:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* message = [command.arguments objectAtIndex:0];
-
-    if (message != nil && [message length] > 0) {
-        message = @"来自iOS的chart消息......";
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
 
 @end
  
