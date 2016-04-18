e87ceba50942123f48b6f05cfdc96b71eae86848
linxiaolong <linxiaolong@damaiapp.com>
2016-04-18 15:35:47 +0800

    [Add]daily commit with date 2016年04月18日 15:35:47

--- www/dmhuanxin.js
+++ www/dmhuanxin.js
@@ -1,6 +1,7 @@
 var exec = require('cordova/exec');
 
-var dmhuanxin= {
+var dmhuanxin = {
+	
 	// echo接口，msg为传入的参数，success, error为回调函数
 　　echo : function(msg, success, error) {
 　　　　exec(success, error, "dmhuanxin", "echo", [msg]);
